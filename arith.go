9b551b49fad144a4f25d6e01e7dcddf5b7fb7641
Josh Pattman <Josh.pattman@gmail.com>
2024-02-14 00:36:14 +0000

    Added some arithmetic

---
+++ arith.go
@@ -0,0 +1,57 @@
+package functional
+
+func Sum[T Number](vs []T) T {
+	sum := T(0)
+	for _, v := range vs {
+		sum += v
+	}
+	return sum
+}
+
+func Mean[T Number](vs []T) T {
+	return Sum(vs) / T(len(vs))
+}
+
+func Max[T Number](vs []T) T {
+	max := vs[0]
+	for _, v := range vs {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func Min[T Number](vs []T) T {
+	min := vs[0]
+	for _, v := range vs {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func ArgMax[T Number](vs []T) int {
+	index := 0
+	max := vs[0]
+	for i, v := range vs {
+		if v > max {
+			max = v
+			index = i
+		}
+	}
+	return index
+}
+
+func ArgMin[T Number](vs []T) int {
+	index := 0
+	min := vs[0]
+	for i, v := range vs {
+		if v < min {
+			min = v
+			index = i
+		}
+	}
+	return index
+}
